ddf3a23a6fdd47dc62eb3697ccea91e052abd6d9
Marte Julie <m.j.satra@fys.uio.no>
2019-07-02 15:11:57 +0200

    figure 4 reduced to 3x3

--- figures/plot_figure4.py
+++ figures/plot_figure4.py
@@ -11,19 +11,18 @@ from set_style import set_style
 
 set_style('article', w=1, h=2)
 
-filename = np.array([['../data/figure4/bias_a', '../data/figure4/bias_b', '../data/figure4/bias_c', '../data/figure4/bias_d'], ['../data/figure4/bias_e', '../data/figure4/bias_f', '../data/figure4/bias_g', '../data/figure4/bias_h'], ['../data/figure4/bias_i', '../data/figure4/bias_j', '../data/figure4/bias_k',  '../data/figure4/bias_l'],  ['../data/figure4/bias_m', '../data/figure4/bias_n', '../data/figure4/bias_o', '../data/figure4/bias_p']])
+filename = np.array([['../data/figure4/bias_a', '../data/figure4/bias_b', '../data/figure4/bias_c'], ['../data/figure4/bias_d', '../data/figure4/bias_e', '../data/figure4/bias_f'], ['../data/figure4/bias_g', '../data/figure4/bias_h', '../data/figure4/bias_i']])
 
-panel = np.array([['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'J', 'K',  'L'],  ['M', 'N', 'O', 'P']])
+panel = np.array([['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'])
 
-fig, axarr = plt.subplots(4,4)
+fig, axarr = plt.subplots(3,3)
 plt.draw()
-p0 = axarr[3,0].get_position().get_points().flatten()
-p1 = axarr[3,1].get_position().get_points().flatten()
-p2 = axarr[3,2].get_position().get_points().flatten()
-p3 = axarr[3,3].get_position().get_points().flatten()
+p0 = axarr[2,0].get_position().get_points().flatten()
+p1 = axarr[2,1].get_position().get_points().flatten()
+p2 = axarr[2,2].get_position().get_points().flatten()
 
-for i in range(0,4):
-    for j in range(0,4):
+for i in range(0,3):
+    for j in range(0,3):
 
         # load data
         data = sio.loadmat(filename[i,j])
@@ -48,7 +47,7 @@ for i in range(0,4):
             axarr[i,j].text(-0.1, 1.1, panel[i,j], transform=axarr[i,j].transAxes, fontsize=16, fontweight='bold', va='top', ha='right')
 
         # first colorbar
-        if j == 0 and i == 3:
+        if j == 0 and i == 2:
             ax_cbar0 = fig.add_axes([p0[0], 0.075, p0[2]-p0[0], 0.015])
             cbar = plt.colorbar(cset1, ticks=[-10,-5,0,5,10], cax=ax_cbar0, orientation='horizontal', extend='both')
             cbar.ax.set_xticklabels([-10, -5, 0, 5, 10], rotation=45)
@@ -69,18 +68,18 @@ for i in range(0,4):
         vessel = mpatches.Circle((max(x)/2, max(y)/2), 6./141, facecolor='black')
         axarr[i,j].add_patch(vessel)
 
-for i in range(1,4):
-    for j in range(0,4):
+for i in range(1,3):
+    for j in range(0,3):
         plt.setp(axarr[i,j].get_xticklabels(), visible=False)
-for i in range(0,4):
-    for j in range(1,4):
+for i in range(0,3):
+    for j in range(1,3):
         plt.setp(axarr[i,j].get_yticklabels(), visible=False)
 
 # d_data and q_data 
-for i in range(0,4):
+for i in range(0,3):
     a = sio.loadmat(filename[i,0])
-    if i == 3:
-        axarr[i,0].set_ylabel('$\hat{d}\mathrm{_{data}}=$\n$%.2f$' % a['d_data'], labelpad=10)
+    if i == 0:
+        axarr[i,0].set_ylabel('$\hat{d}\mathrm{_{data}}=$\n$%.4f$' % a['d_data'], labelpad=10)
     else:
         axarr[i,0].set_ylabel('$\hat{d}\mathrm{_{data}}=$\n$%.3f$' % a['d_data'], labelpad=10)
     a = sio.loadmat(filename[0,i])
@@ -93,8 +92,7 @@ for i in range(0,4):
 # colorbars
 ax_cbar1 = fig.add_axes([p1[0], 0.075, p1[2]-p1[0], 0.015])
 ax_cbar2 = fig.add_axes([p2[0], 0.075, p2[2]-p2[0], 0.015])
-ax_cbar3 = fig.add_axes([p3[0], 0.075, p3[2]-p3[0], 0.015])
-for ax in [ax_cbar1, ax_cbar2, ax_cbar3]:
+for ax in [ax_cbar1, ax_cbar2]:
     cbar = plt.colorbar(cset1, ticks=[-100,-50,0,50,100], cax=ax, orientation='horizontal', extend='both')
     cbar.ax.set_xticklabels([-100, -50, 0, 50, 100], rotation=45)
     cbar.ax.tick_params(direction='in')
