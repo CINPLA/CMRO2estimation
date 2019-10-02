7796f5603dce5154f0740ed2cead5de52dc91612
Marte Julie <m.j.satra@fys.uio.no>
2019-10-02 09:58:51 +0200

    bash script for plotting all figures

---
+++ figures/plot_all.sh
@@ -0,0 +1,12 @@
+
+echo "Plot all figures..."
+python plot_figure1.py
+python plot_figure2.py
+python plot_figure3.py
+python plot_figure4.py
+python plot_figure5.py
+python plot_figure6.py
+python plot_figure7.py
+python plot_figure8.py
+python plot_figure9.py
+echo "Figures done."
